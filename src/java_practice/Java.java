a9e268af77565a4f0d8b598aba32189ca280c05a
FIRST_NAME LAST_NAME <shiva.kumar@cognitivzen.com>
2023-09-13 18:44:15 +0530

    third commit

--- src/java_practice/Java.java
+++ src/java_practice/Java.java
@@ -3,19 +3,9 @@ package java_practice;
 public class Java {
 	public static void main(String args[]) {
 		int n=5;
-		for(int i=1;i<=n;i++) {
-			for(int j=i;j<=n;j++) {
-				System.out.print(" *");
-			
-			}System.out.println(" ");
-		}
-	
-		for(int i=1;i<=n;i++) {
-			for(int j=i;j<=n;j++) {
-				System.out.print(" *");
-			
-			}System.out.println(" ");
-		}
+
+	  System.out.println();
+
 
           
 
